aa3283126a8261ff88bf28b79deaac5de88059c6
ouin85 <bouinten@yahoo.fr>
2021-04-02 23:06:36 +0200

    change asset_exists? method in order to fix bug with asset pipeline in Heroku

--- app/helpers/asset_helper.rb
+++ app/helpers/asset_helper.rb
@@ -1,7 +1,19 @@
 module AssetHelper
   def asset_exists?(path)
-    if Rails.env.production?  
-      Rails.application.assets_manifest.find_sources(path) != nil
+    if Rails.env.production?
+      all_assets = Rails.application.assets_manifest.find_sources(path)
+      
+      if all_assets
+        keys = Rails.application.assets_manifest.assets.keys
+        
+        if keys.include?(path)
+          true
+        else
+          false
+        end
+      else
+        false
+      end
     else
       Rails.application.assets.find_asset(path) != nil
     end
