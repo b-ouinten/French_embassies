65e77bdf4781d98c106032c9b4b835b41a3d35c8
ouin85 <bouinten@yahoo.fr>
2021-04-04 02:24:41 +0200

    mobile freindly

--- app/helpers/geocoding_helper.rb
+++ app/helpers/geocoding_helper.rb
@@ -15,7 +15,6 @@ module GeocodingHelper
   
   def street_view(center)
     "https://maps.googleapis.com/maps/api/streetview?size=450x300&location=#{center.latitude},#{center.longitude}
-    &fov=80&heading=70&pitch=0
-    &key=#{ENV["GOOGLE_MAPS_API_KEY"]}"
+    &fov=80&heading=70&pitch=0&key=#{ENV["GOOGLE_MAPS_API_KEY"]}"
   end
 end
